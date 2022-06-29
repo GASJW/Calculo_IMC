b28c41c7a6382b8e9c063bba5b16402819815cc7
Guilherme <guilhermeguto5@gmail.com>
2022-06-28 23:15:42 -0300

    Calculo IMC

---
+++ README.md
@@ -0,0 +1,3 @@
+# Calculo_IMC
+
+Exercicio de prática para calcular o Indice de Massa Corporal e retornar o estado em que a pessoa se encontra.
---
+++ assets/js/main.js
@@ -0,0 +1,77 @@
+function meuEscopo() {
+  const form = document.querySelector("#form");
+  const result = document.querySelector("#result");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const inputPeso = event.target.querySelector("#peso");
+    const inputAltura = event.target.querySelector("#altura");
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    if (!peso || !altura) {
+      setResultado("Entrada inválida", false);
+      return;
+    }
+
+    const imc = getIMC(peso, altura);
+    const nivelImc = getNivelIMC(imc);
+
+    const msg = `Seu IMC é ${imc} (${nivelImc}).`;
+
+    setResultado(msg, true);
+  });
+
+  function getNivelIMC(imc) {
+    const nivel = [
+      "Abaixo do peso",
+      "Peso normal",
+      "Sobrepeso",
+      "Obesidade grau 1",
+      "Obesidade grau 2",
+      "Obesidade grau 3",
+    ];
+
+    if (imc >= 39.9) return nivel[5];
+
+    if (imc >= 34.9) return nivel[4];
+
+    if (imc >= 29.9) return nivel[3];
+
+    if (imc >= 24.9) return nivel[2];
+
+    if (imc >= 18.5) return nivel[1];
+
+    if (imc < 18.5) return nivel[0];
+  }
+
+  function getIMC(peso, altura) {
+    const imc = peso / (altura * altura);
+    return imc.toFixed(2);
+  }
+
+  function criaParagrafo() {
+    const p = document.createElement("p");
+    return p;
+  }
+
+  function setResultado(msg, isValid) {
+    const result = document.querySelector("#result");
+    result.innerHTML = "";
+
+    const p = criaParagrafo();
+
+    if (isValid) {
+      p.classList.add("paragrafo-resultado");
+    } else {
+      p.classList.add("bad");
+    }
+
+    p.innerHTML = msg;
+    result.appendChild(p);
+  }
+}
+
+meuEscopo();
